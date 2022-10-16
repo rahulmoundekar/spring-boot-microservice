e1158b7b4d16a44819d474b46de0216b438a0677
Rahul Moundekar <rahulmoundekar44@gmail.com>
2022-10-16 16:56:30 +0530

    user-service-setup

---
+++ user-service/src/main/java/com/app/service/UserService.java
@@ -0,0 +1,9 @@
+package com.app.service;
+
+import com.app.entity.User;
+
+public interface UserService {
+
+	public User getUser(Long id);
+
+}
---
+++ user-service/src/main/java/com/app/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.app.service;
+
+import org.springframework.stereotype.Service;
+
+import com.app.entity.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	// fake user list
+
+	List<User> list = List.of(new User(1311L, "Durgesh Tiwari", "23525625"), new User(1312L, "Ankit Tiwari", "99999"),
+			new User(1314L, "Ravi Tiwari", "888"));
+
+	@Override
+	public User getUser(Long id) {
+		return list.stream().filter(user -> user.getUserId().equals(id)).findAny().orElse(null);
+	}
+}
